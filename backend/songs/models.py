54cc3b377f51aa341cb9839bd9cb1790f1ebe2db
Akira Satoh <akira31758421@gmail.com>
2025-04-28 09:23:18 +0700

    add price for song

--- backend/songs/models.py
+++ backend/songs/models.py
@@ -19,6 +19,8 @@ class Song(models.Model):
     genres = models.ManyToManyField(Genres)
     is_deleted = models.BooleanField(default=False)
     release_date = models.DateField(null=True, blank=True, default=timezone.now)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
 
     def __str__(self):
         return self.song_name
