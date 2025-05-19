c9ce3a3a6384605dce037d1f5a7c6ecd5f294ce4
Akira Satoh <akira31758421@gmail.com>
2025-05-19 20:45:00 +0700

    merge main to develop

--- backend/playlists/views.py
+++ backend/playlists/views.py
@@ -10,10 +10,8 @@ class PlaylistListCreateView(generics.ListCreateAPIView):
     queryset = Playlist.objects.filter(is_deleted=False)
     serializer_class = PlaylistSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    # Cho phép tìm kiếm
     filter_backends = [filters.SearchFilter]
-    search_fields = ['playlist_name', 'description']  # Các trường cho phép tìm kiếm
+    search_fields = ['playlist_name', 'description']  
 
     def perform_create(self, serializer):
         playlist = serializer.save(user=self.request.user)
@@ -22,13 +20,12 @@ class PlaylistListCreateView(generics.ListCreateAPIView):
             playlist.song.set(songs)
 
     def get_queryset(self):
-        # Mặc định chỉ trả về playlist chưa xóa
         return Playlist.objects.filter(is_deleted=False)
 
 class PlaylistRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Playlist.objects.filter(is_deleted=False)
     serializer_class = PlaylistSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Chỉ cho phép user đã đăng nhập
+    permission_classes = [permissions.IsAuthenticated]
 
     def destroy(self, request, *args, **kwargs):
         playlist = self.get_object()
